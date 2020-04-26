e321097bb5c5c4929dd01be0c5f5a99051fe3e33
Khan Hooi <khanhooi@pop-os.localdomain>
2020-04-26 23:10:37 +1000

    Aded new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { GameBoardComponent } from './game-board/game-board.component';
 import { BoardColumnComponent } from './board-column/board-column.component';
 import { BoardSquareComponent } from './board-square/board-square.component';
 import { PlayerDisplayComponent } from './player-display/player-display.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { PlayerDisplayComponent } from './player-display/player-display.componen
     GameBoardComponent,
     BoardColumnComponent,
     BoardSquareComponent,
-    PlayerDisplayComponent
+    PlayerDisplayComponent,
+    NavigationBarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
