89c7b6905a970ce043a4632403f72e50deaf1bf5
Khan Hooi <khanhooi@pop-os.localdomain>
2020-04-28 13:36:33 +1000

    Added css grid to main layout

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,17 +1,35 @@
-
 import { GameService } from '../services/game.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
-  styleUrls: ['./game-board.component.css']
+  styleUrls: ['./game-board.component.css'],
 })
 export class GameBoardComponent implements OnInit {
+  constructor(public gameService: GameService) {}
+  ngOnInit(): void {}
 
-  constructor(public gameService: GameService) { }
-  ngOnInit(): void {
+  /* To ensure that all columns have the same dimensions,
+     we need to manually divide the available space to the number of columns.
+  */
+  getTableWidth(containerOffsetWidth: number, containerOffsetHeight: number): number {
+    const numberOfColumns = this.gameService.board.length;
+    const numberOfRows = this.gameService.board[0].length;
 
+    let squareSize = Math.floor(containerOffsetWidth / numberOfColumns) + 1;
+    let height: number;
+    do {
+      squareSize--;
+      console.log(`something is happening: ${squareSize}`);
+
+      // square size is number of rows + 1 because there is the equivalent of an extra row for the
+      // hint token.
+      height = squareSize * ( numberOfRows + 2 );
+    } while (height > containerOffsetHeight);
+    return squareSize * numberOfColumns;
   }
-
+  onResize(event): void{
+    event.target.innerWidth;
+  }
 }
